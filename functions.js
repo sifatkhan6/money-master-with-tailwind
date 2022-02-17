ddd7276b19ea38ffd0f256bd1fa973aae10e387c
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-02-17 21:05:57 +0800

    error added

--- functions.js
+++ functions.js
@@ -26,12 +26,29 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const incomeInput = document.getElementById('income-input');
     const incomeAmountText = incomeInput.value;
     incomeAmount = parseFloat(incomeAmountText);
-    const finalCostAmount = getTotalExpenses(incomeAmount);
 
+    if(isNaN(incomeAmount) || incomeAmount < 0){
+        const error = document.getElementById('error-message-1');
+        error.style.display = 'block';
+        incomeInput.value = '';
+        document.getElementById('food-cost').value = '';
+        document.getElementById('house-rent-cost').value = '';
+        document.getElementById('cloths-cost').value = '';
+        document.getElementById('total-expenses').value = '';
+        document.getElementById('remaining-balance').value = '';
+    }
+    
+    else{
+    const finalCostAmount = getTotalExpenses(incomeAmount);
+    
     // remaining balance 
     remainingBalance = incomeAmount - finalCostAmount;
     remainingBalanceOutput = document.getElementById('remaining-balance');
     remainingBalanceOutput.innerText = remainingBalance;
+
+    const error = document.getElementById('error-message-1');
+    error.style.display = 'none';
+    }
 })
 
 // Saving Button 
@@ -50,7 +67,7 @@ document.getElementById('save-button').addEventListener('click', function(){
         savingParcentText = savingParcentInputText.value;
         savingParcentAmount = parseFloat(savingParcentText);
         savingParcent = savingParcentAmount / 100;
-        savingAmount = remainingBalance * savingParcent;
+        savingAmount = incomeAmount * savingParcent;
 
         // saving amount output 
         savingAmountOutput = document.getElementById('saving-amount');
