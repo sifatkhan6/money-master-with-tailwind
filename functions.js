91223618894ef930c85a6fffeaa91326d8137b0a
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-02-17 23:34:32 +0800

    error handling added

--- functions.js
+++ functions.js
@@ -12,12 +12,24 @@ function getTotalExpenses(){
     const clothsCostAmountText = clothsCostInput.value;
     const clothsCostAmount = parseFloat(clothsCostAmountText);
 
+    if(isNaN(foodCostAmount) || foodCostAmount < 0 || isNaN(houseRentAmount) || houseRentAmount < 0 || isNaN(clothsCostAmount) || clothsCostAmount < 0){
+        const error2 = document.getElementById('error-message2');
+        error2.style.display = 'block';
+        document.getElementById('remaining-balance').innerText = '';
+        document.getElementById('total-expenses').innerText = '';
+    }
+
+    else{
     // total expenses 
     const totalCost = foodCostAmount + houseRentAmount + clothsCostAmount;
     const totalCostOutput = document.getElementById('total-expenses');
     totalCostOutput.innerText = totalCost;
 
+    const error2 = document.getElementById('error-message2');
+    error2.style.display = 'none';
+
     return totalCost;
+    }
 }
 
 // Calculate Button 
@@ -28,23 +40,34 @@ document.getElementById('calculate-button').addEventListener('click', function()
     incomeAmount = parseFloat(incomeAmountText);
 
     if(isNaN(incomeAmount) || incomeAmount < 0){
-        const error = document.getElementById('error-message-1');
-        error.style.display = 'block';
-        incomeInput.value = '';
-        document.getElementById('food-cost').value = '';
-        document.getElementById('house-rent-cost').value = '';
-        document.getElementById('cloths-cost').value = '';
-        document.getElementById('total-expenses').value = '';
-        document.getElementById('remaining-balance').value = '';
+        const error1 = document.getElementById('error-message-1');
+        error1.style.display = 'block';
+        document.getElementById('remaining-balance').innerText = '';
+        document.getElementById('total-expenses').innerText = '';
     }
     
     else{
     const finalCostAmount = getTotalExpenses(incomeAmount);
     
     // remaining balance 
-    remainingBalance = incomeAmount - finalCostAmount;
-    remainingBalanceOutput = document.getElementById('remaining-balance');
-    remainingBalanceOutput.innerText = remainingBalance;
+    const remainingBalance = incomeAmount - finalCostAmount;
+    const remainingBalanceOutput = document.getElementById('remaining-balance');
+        if(incomeAmount < finalCostAmount){
+            const error3 = document.getElementById('error3');
+            error3.style.display = 'block';
+            incomeInput.value = '';
+            document.getElementById('food-cost').value = '';
+            document.getElementById('house-rent-cost').value = '';
+            document.getElementById('cloths-cost').value = '';
+            document.getElementById('total-expenses').value = '';
+            document.getElementById('remaining-balance').innerText = '';
+            document.getElementById('total-expenses').innerText = '';
+        }
+        else{
+            remainingBalanceOutput.innerText = remainingBalance;
+            const error3 = document.getElementById('error3');
+            error3.style.display = 'none';
+        }
 
     const error = document.getElementById('error-message-1');
     error.style.display = 'none';
@@ -69,12 +92,22 @@ document.getElementById('save-button').addEventListener('click', function(){
         savingParcent = savingParcentAmount / 100;
         savingAmount = incomeAmount * savingParcent;
 
+        if(savingAmount > remainingBalance){
+            const error4 = document.getElementById('error-message-4');
+            error4.style.display = 'block';
+        }
+
+        else{
         // saving amount output 
         savingAmountOutput = document.getElementById('saving-amount');
         savingAmountOutput.innerText = savingAmount;
+
+        const error4 = document.getElementById('error-message-4');
+        error4.style.display = 'none';
 
         // remaining balance after saving 
         remainingBalanceAfterSaving = remainingBalance - savingAmount;
         remainingBalanceAfterSavingOutput = document.getElementById('remaining-balance-afterSaving');
         remainingBalanceAfterSavingOutput.innerText = remainingBalanceAfterSaving;
+        }
 })
